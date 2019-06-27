f5059985d78ef6a8329c5c46038b645fe758c8c7
robert <robert@gobear.com>
2019-06-27 17:29:01 +0700

    adding script to start microservices locally

---
+++ template.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+serviceName=dms2-template
+path=$serviceName
+
+echo "Build & packaging... $serviceName"
+
+cd $path
+mvn clean install -DskipTests
+
+echo Packaging...
+mvn package -DskipTests
+
+echo Go back ...
+cd ..
+
+echo Install package into docker container...
+docker-compose build --force-rm $serviceName
+
+echo Starting api-gateway...
+docker-compose up --remove-orphans $serviceName
+
